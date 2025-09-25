a0a7c157bec088f753aa0647bf22b88609aa4239
tanuxxx <tanmaymp92@gmail.com>
2025-09-25 13:26:02 +0530

    String Started

---
+++ src/Strings/A1_removeOuterParentheses.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class A1_removeOuterParentheses {
+    public static  String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count!=0){
+                ans.append(s.charAt(i));
+            }
+            if(s.charAt(i) == '('){
+                count++;
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the string= ");
+        String s = sc.nextLine();
+
+        String result = removeOuterParentheses(s);
+        System.out.print("result= "+result);
+
+    }
+
+}
