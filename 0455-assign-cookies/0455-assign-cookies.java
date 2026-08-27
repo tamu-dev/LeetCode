d05e357e857aa94c5b17bc5fbee20f768bf6d1f0
Tamu <tanmaymp92@gmail.com>
2026-08-27 16:31:50 +0530

    Time: 14 ms (92.15%), Space: 51.6 MB (37.14%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n = g.length;
+        int m = s.length;
+        int l=0 , r=0;
+        while(l<m && r<n){
+            if(g[r] <= s[l]){
+                r = r +1;
+            }
+            l = l+1;
+        }
+        return r;
+    }
+}
